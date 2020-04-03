bab004462265fa1e2f66ea6bf03d2aac915aae1f
Duvan Rosero <duvanrosero1995agmail.com>
2020-04-03 16:51:43 -0500

    añadimos el texto final con su respectivo scroll

--- lib/view/inicio.dart
+++ lib/view/inicio.dart
@@ -11,17 +11,16 @@ class Inicio extends StatelessWidget {
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Image.network("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
-              _seccion1(),
-              _acciones()
-             
-
-
-
-            ],
+        body: SingleChildScrollView(
+                  child: Container(
+            child: Column(
+              children: <Widget>[
+                Image.network("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+                _seccion1(),
+                _acciones(),
+                _crearTexto()
+              ],
+            ),
           ),
         )
       );
@@ -30,7 +29,7 @@ class Inicio extends StatelessWidget {
 
   Widget _seccion1(){
     return  Container(
-            padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0),
+            padding: EdgeInsets.symmetric(horizontal: 15.0,vertical: 15.0),
             child: Row(
               children: <Widget>[
                 Expanded(
@@ -73,6 +72,19 @@ class Inicio extends StatelessWidget {
           Text(texto,style: TextStyle(color: Colors.blue),)
         ],
       );
+  }
+
+  Widget _crearTexto(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15.0,vertical: 15.0),
+      child: Column(
+        children: <Widget>[
+          Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32."),
+          Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32."),
+        ],
+      ),
+    );
+
   }
 
 }
